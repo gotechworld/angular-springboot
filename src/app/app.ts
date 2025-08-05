cb92f8fa80aa05c9495d8ef3c852bfb61847d8b1
petrugiurca <petru.giurca@pm.me>
2025-08-05 11:00:34 +0300

    Changed the app.ts title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Expert Achizitii Publice');
+  protected readonly title = signal('aniagency');
 }
